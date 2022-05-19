4d78ac5a482cf5dedeac316e6c69637f76357f06
Illya <71638255+ilky03@users.noreply.github.com>
2022-05-19 15:01:19 +0300

    Add files via upload

--- flappybird/js/game_new.js
+++ flappybird/js/game_new.js
@@ -9,11 +9,12 @@ class Character {
         this.name = name;
         this.health = health;
         this.speed = speed;
-        this.xPos = 100;
-        this.yPos = 150;
         this.skin = new Image();
         this.skin.src = skinSrc;
+        this.xPos = 100;
+        this.yPos = 150;
     }
+
 }
 
 class Player {
@@ -54,10 +55,10 @@ class RatingTable {
 
 }
 
-let bg1 = new Map('img/bg-1.jpg', 1.5, 'img/pipeUp.png', 'img/pipeBottom.png');
+let bg1 = new Map('img/bg-1.jpg', 1.5, 'img/pipeUp_bg-1.png', 'img/pipeBottom_bg-1.png');
 
 let bird = new Character('bird', 3, 25, 'img/bird.png');
-let gap = 90;
+let gap = 100;
 
 document.addEventListener("keydown", moveUp);
 
@@ -65,43 +66,85 @@ function moveUp() {
     bird.yPos -= 25;
 }
 
+let hp = new Image();
+hp.src = "img/hp.png";
+let hp2 = new Image();
+hp2.src = "img/hp2.png";
+
+let score = 0;
+let flag = 0;
 let pipe = [];
 pipe[0] = {
     x: cvs.width,
-    y: 0
+    y: -300
 }
-let a = 500;
+
 function draw() {
     ctx.drawImage(bg1.map, 0, 0, cvs.width, cvs.height)
 
     for (let i = 0; i < pipe.length; i++) {
         ctx.drawImage(bg1.pipeUp, pipe[i].x, pipe[i].y);
-        ctx.drawImage(bg1.pipeBottom, pipe[i].x, pipe[i].y + 400);
+        ctx.drawImage(bg1.pipeBottom, pipe[i].x, pipe[i].y + bg1.pipeUp.height + gap);
 
         pipe[i].x--;
 
         if (pipe[i].x === cvs.width-200) {
             pipe.push({
                 x: cvs.width,
-                y: Math.trunc(Math.random() * 100)
+                y: Math.trunc(Math.random() * 300) - 300
             })
         }
-       // pipe[i].x--;
 
-       // if (pipe[i].x === 1200) {
-      //      pipe.push({
-     //           x: cvs.width,
-     //           y: Math.floor(Math.random() * bg1.pipeUp.height) - bg1.pipeUp.height
-      //      });
+        if(((bird.xPos + bird.skin.width > pipe[i].x
+                && bird.xPos < pipe[i].x + bg1.pipeUp.width
+                && (bird.yPos < pipe[i].y + bg1.pipeUp.height
+                    || bird.yPos + bird.skin.height > pipe[i].y + bg1.pipeUp.height + gap))
+            || bird.yPos > cvs.height || bird.yPos < 0) && flag === 0) {
+            if(bird.health > 0) {
+                bird.health--;
+            } else {
+                alert('freeze');
+                location.reload();
+            }
+            flag = 1;
+            setTimeout(() => flag = 0, 1500);
         }
 
-        ctx.drawImage(bird.skin, bird.xPos, bird.yPos);
+        if (pipe[i].x === 100) {
+            score++;
+        }
 
-        bird.yPos += bg1.gravity;
+        if (bird.health === 3) {
+            ctx.drawImage(hp, 10, 10);
+            ctx.drawImage(hp, 50, 10);
+            ctx.drawImage(hp, 90, 10);
+        } else if (bird.health === 2) {
+            ctx.drawImage(hp, 10, 10);
+            ctx.drawImage(hp, 50, 10);
+            ctx.drawImage(hp2, 90, 10);
+        } else if(bird.health === 1) {
+            ctx.drawImage(hp, 10, 10);
+            ctx.drawImage(hp2, 50, 10);
+            ctx.drawImage(hp2, 90, 10);
+        } else {
+            ctx.drawImage(hp2, 10, 10);
+            ctx.drawImage(hp2, 50, 10);
+            ctx.drawImage(hp2, 90, 10);
+        }
 
-        requestAnimationFrame(draw);
+    }
+
+    ctx.drawImage(bird.skin, bird.xPos, bird.yPos);
+
+    bird.yPos += bg1.gravity;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "15px PixelFont";
+    ctx.fillText("SCORE: " + score, 10, cvs.height - 20);
+
+    requestAnimationFrame(draw);
 }
 
-let newbg = new Image();
-newbg.src = 'img/bg-1.jpg';
-newbg.onload = draw;
+let img = new Image();
+img.src = "img/bg-1.jpg";
+img.onload = draw;
